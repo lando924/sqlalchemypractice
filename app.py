c1806e795467602ce0664f999e79981d871ba97a
lando924 <ojcastaneda9@gmail.com>
2024-12-28 00:22:10 -0500

    SQLAlchemy practice

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
-app.config['SECRET_KEY'] = 'chickenzarecool122424'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
+
+
+app.config['SECRET_KEY'] = "chickenzarecool1234"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
@@ -17,3 +21,12 @@ def home_page():
     """Shows home page"""
     return render_template('home.html')
 
+ 
+if __name__ == "__main__":
+    with app.app_context():
+        try:
+            result = db.create_all()
+        except Exception as e:
+            print(f"Error querying the databse:{e}")
+
+    app.run
--- models.py
+++ models.py
@@ -3,13 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
-    """Connect to database"""
-    with app.app_context():
-        db.app = app
-        db.init_app(app)
-        db.create_all()
+    """Connects the database to the Flask app"""
+    db.app = app
+    db.init_app(app)
 
+# MODELS GO BELOW!
+    
+class Pet(db.Model):
 
+    __tablename__ = "pets"
 
-# MODELS GO BELOW! ESSENTIALLY SCHEMA
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    name = db.Column(db.String(50),
+                   nullable=False,
+                   unique=True)
+    
+    species = db.Column(db.String(30), nullable=True)
+
+    hunger = db.Column(db.Integer, nullable=False, default=20)
+
